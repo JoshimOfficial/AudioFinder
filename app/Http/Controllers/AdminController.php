2324b8d6620947b8be62bb391186154c840fd9a9
Joshim Uddin <idkhs8211@gmail.com>
2023-10-06 00:17:09 +0600

    added cookies

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,7 +6,43 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    function GetAPI(Request $request) {
-        return response()->json(['message' => 'Hello, this is your text!']);
-    }
+   public function GetCookies(Request $request) {
+    // Initialize cURL session
+$ch = curl_init();
+
+// Set the URL to the resource you want to retrieve
+$url = "https://dailywebcookies1.blogspot.com/p/semrush.html";
+curl_setopt($ch, CURLOPT_URL, $url);
+
+// Set cURL options
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Return the response as a string
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // Follow redirects, if any
+// You can add more options like headers, user-agent, etc., as needed
+
+// Execute cURL session and store the response in $response
+$response = curl_exec($ch);
+
+// Check for cURL errors
+if(curl_errno($ch)){
+    echo 'Curl error: ' . curl_error($ch);
+}
+
+// Close cURL session
+curl_close($ch);
+
+$data = $response;
+
+
+$pattern = '/<div[^>]*\s+id\s*=\s*(?:"|\')?postBody(?:"|\')[^>]*>(.*?)<\/div>/si';
+
+// Use preg_match to find the content within the matched <div>
+if (preg_match($pattern, $data, $matches)) {
+    $postBodyContent = $matches[1]; // The content inside the <div> with id="postBody"
+    echo $postBodyContent; // Output or use $postBodyContent as needed
+} else {
+    echo "No matching <div id=\"postBody\"> found.";
+}
+
+
+   }
 }
